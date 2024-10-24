7c1db92207e8a7e764e0ada282f30f5ec2ca19e4
Disha Parale <99310162+paralexdisha7@users.noreply.github.com>
2024-10-24 15:10:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ProductList from './components/ProductList';
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl text-center pt-10">Inventory Management Application</h1>
+      <h1 className="text-3xl text-center pt-10">Store Supply Chain Management Application</h1>
       <AddProduct />
       <ProductList />
     </div>
